ea6ff066c3fcd6d2eebaa9187e336a8c6218df35
Tomoya Suzuki <suzutomo@toralab.org>
2016-01-18 00:08:10 +0900

    trainsition利用

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ google.load("feeds", "1");
 
 addEventListener('DOMContentLoaded', function(){
     /*RSSの一覧をリストで表示*/
-    getRSS('http://summer.ics.nitech.ac.jp/apps/news/rss/', function(data) {
+    getRSS('http://staff.hatenablog.com/rss', function(data) {
         showData(data);
     });
 });
@@ -26,22 +26,19 @@ function getRSS(url, callback) {
 
 /* 取得したJSONをvue.jsで表示 */
 function showData(data) {
-    var entries = data.feed.entries;
-
-    /* 各記事のコンポーネント */
-    var Entry = Vue.extend({
+    Vue.component('my-entry', {
         template: '#my-entry-template',
-        props: ['entry'],
+        props: ['item'],
         computed: {
             date: function() {
-                return new Date(this.entry.publishedDate).toLocaleDateString("ja-JP", {
+                return new Date(this.item.publishedDate).toLocaleDateString("ja-JP", {
                     year: 'numeric',
                     month: '2-digit',
                     day: '2-digit'
                 });
             },
             new: function() {
-                var date = new Date(this.entry.publishedDate);
+                var date = new Date(this.item.publishedDate);
                 var now = new Date();
                 var diff = new Date(now - date);
                 return diff.getUTCMonth() < 1;
@@ -49,29 +46,11 @@ function showData(data) {
         }
     });
 
-    /* 記事リストのコンポーネント */
-    var Reader = Vue.extend({
-        template: '#my-reader-template',
-        components: {
-            'my-entry': Entry
-        },
-        props: ['entries']
-    });
-
-    new Vue({
+    var entries = data.feed.entries;
+    vm = new Vue({
         el: '#app',
         data: {
-            entries: []
-        },
-        components: {
-            'my-reader': Reader
-        },
-        ready() {
-            entries.forEach(function(item) {
-                this.entries.push(item);
-            }.bind(this));
+            entries: entries
         }
     });
-
-
 }
---
+++ src/models/feed.js
@@ -0,0 +1,16 @@
+'use strict';
+import FeedProxy from 'models/feed.js';
+import RssComponent from 'components/rss.js';
+import goog from 'lib/jsapi.js';
+
+class FeddProxy {
+    constrcutor(url) {
+        this.url = url;
+
+
+    }
+
+    get() {
+        
+    }
+}
